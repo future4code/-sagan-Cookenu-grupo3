3242273711505bbb96bc14df770164ddd57fccdc
EduCosta85 <59846713+EduCosta85@users.noreply.github.com>
2020-05-15 13:44:06 -0300

    endpoint de apagar receita

---
+++ src/endpoints/deleteRecipeEndPoint.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { Authenticator } from "../services/Authenticator";
+import { BaseDatabase } from "../data/BaseDataBase";
+import { RecipesDataBase } from "../data/RecipesDataBase";
+
+export const deleteRecipeEndPoint = async (req: Request, res: Response) => {
+    try {
+        const token = req.headers.authorization as string;
+        const userData = await new Authenticator().getData(token);
+        const recipeOwner = await new RecipesDataBase().getRecipeCreatorByRecipeId(Number(req.params.id))
+        console.log(userData.id)
+        console.log(recipeOwner)
+        console.log(recipeOwner != userData.id)
+        console.log(userData.role)
+        console.log(userData.role != "admin")
+        if((recipeOwner != userData.id) || (userData.role == "admin")){
+            throw new Error("Você precisa ser o criador da receita ou um administrador para poder remove-la")
+        }
+        const deleteRecipe = await new RecipesDataBase().deleteRecipe(Number(req.params.id))
+
+        res.sendStatus(200)
+    } catch (err) {
+        res.status(400).send({
+            message: err.message,
+        });
+    }
+    await BaseDatabase.destroyConnection()
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config()
-
 import express from "express";
 import { AddressInfo } from "net";
 import { signupEndPoint } from "./endpoints/signupEndPoint";
-
 import { getOwnProfileEndPoint } from "./endpoints/getOwnProfileEndpoint";
 import { getOtherProfileEndPoint } from "./endpoints/getOtherProfileUserEndpoint";
-
 import { loginEndpoint } from "./endpoints/loginEndpoint";
 import { createRecipeEndPoint } from "./endpoints/createRecipeEndpoint";
 import { getRecipeEndPoint } from "./endpoints/getRecipeEndPoint";
@@ -15,6 +12,7 @@ import { feedEndPoint } from "./endpoints/feedEndPoint";
 import { followUserEndPoint } from "./endpoints/followUserEndpoint";
 import { unfolowUserEndPoint } from "./endpoints/unfollowUserEndpoint";
 import { editRecipeEndPoint } from "./endpoints/editRecipeEndPoint";
+import { deleteRecipeEndPoint } from "./endpoints/deleteRecipeEndPoint";
 
 const app = express();
 app.use(express.json());
@@ -23,15 +21,13 @@ app.post("/signup", signupEndPoint)
 app.post("/login", loginEndpoint)
 app.post("/user/follow", followUserEndPoint)
 app.post("/user/unfollow", unfolowUserEndPoint)
-
 app.get("/user/profile", getOwnProfileEndPoint)
 app.get("/user/:id", getOtherProfileEndPoint)
-
 app.post("/recipes/create", createRecipeEndPoint)
 app.get("/recipes/:id", getRecipeEndPoint)
 app.get("/user/feed", feedEndPoint)
 app.put("/recipes/edit/:id", editRecipeEndPoint)
-
+app.delete("/recipes/delete/:id", deleteRecipeEndPoint)
 
 const server = app.listen(process.env.PORT || 3000, () => {
     if (server) {
